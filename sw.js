9461703957135ac84eff34a3b030d3ed23702a99
aimylovesafi-cpu <aimylovesafi@gmail.com>
2026-08-03 11:37:57 +0330

    feat: sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='v22-final'; const FILES=['./','./index.html','./manifest.json','./logo.png']; self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))); self.skipWaiting()}); self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
